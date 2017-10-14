631d14b3a65052b164f72cffbb4305c6b82b4a4b
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2017-10-15 02:04:00 +0600

    Add solution for problem `B - Cutting Carrot` Link: https://codeforces.com/contest/794/problem/B Tags: geometry, math, *1200 Ref: https://codeforces.com/contest/794/submission/31322939

---
+++ codeforces/794/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eps 1e-15
+double eqarea, th1, a, b;
+int h;
+
+double bsearch(double starth, double endh)
+
+{
+    int cnt = 100;
+    double ansh;
+    while(cnt--) {
+        double midh = (starth + endh) / 2.0;
+        b = a - (midh + midh) / tan(th1);
+        double tmpar = 0.5 * (a + b) * midh;
+        if(abs(eqarea - tmpar) <= eps) {
+            ansh = midh;
+        }
+        if(tmpar < eqarea)
+            starth = midh;
+        else
+            endh = midh;
+    }
+
+    return ansh;
+}
+
+int main()
+
+{
+    int n;
+    scanf("%d %d", &n, &h);
+    double area = 0.5 * 1 * h;
+    eqarea = (area) / (n * 1.0);
+    th1 = atan(h / 0.5);
+    a = 1;
+    n--;
+    double dh =  h;
+    int counter = -1;
+    vector <double> st;
+    while(n--) {
+        double ansh;
+        ansh = bsearch(0, dh);
+        dh -= ansh;
+        a = b;
+        st.push_back(dh);
+        counter++;
+    }
+
+    printf("%0.12f", st[counter]);
+    for(int i = counter - 1; i >= 0; i--) {
+        printf(" %0.12f", st[i]);
+    }
+    printf("\n");
+    return 0;
+}
