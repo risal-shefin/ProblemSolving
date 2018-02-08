9bcb43e8d010305de06ae0044097db9f6f9fbe94
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2018-02-08 11:27:00 +0600

    Add solution for problem `C - Cave Painting` Link: https://codeforces.com/contest/922/problem/C Tags: brute force, number theory, *1600 Ref: https://codeforces.com/contest/922/submission/35045064

--- codeforces/922/C.cpp
+++ codeforces/922/C.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check[25];
+bool check[35];
 #define ll long long
 
 int main() {
 	
     ll n, k;
     cin >> n >> k;
-    ll p = min(k, 23LL);
+    ll p = min(k, 25LL);
     for(ll i = 1; i <= p; i++) {
          ll tmp = n % i;
          if(check[tmp]) {
