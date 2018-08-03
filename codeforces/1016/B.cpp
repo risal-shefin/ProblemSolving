8a6dc6b4a9a5764af7fe5071273387ce670808d7
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2018-08-03 23:13:00 +0600

    Add solution for problem `B - Segment Occurrences` Link: https://codeforces.com/contest/1016/problem/B Tags: brute force, implementation, *1300 Ref: https://codeforces.com/contest/1016/submission/41187185

--- codeforces/1016/B.cpp
+++ codeforces/1016/B.cpp
@@ -1,43 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll unsigned long long
-#define mod 3367900313
+#define ll long long
 
-ll hash1[1009], hash2 = 0, pwr[1009];
+ll process[1009][1009];
+
+void findAllOccurances(std::string data, std::string toSearch)
+{
+	// Get the first occurrence
+	size_t pos = data.find(toSearch);
+
+	// Repeat till end is reached
+	while( pos != std::string::npos)
+	{
+
+        for(ll i = pos + toSearch.size() - 1; i < data.length(); i++)
+            process[pos + 1][i + 1] = 1;
+
+		// Get the next occurrence from the current position
+		pos =data.find(toSearch, pos + 1);
+	}
+
+	//cout << 5;
+}
 
 int main()
 
 {
     std::ios_base::sync_with_stdio(false);
-
-    pwr[0] = 1;
-    for(ll i = 1; i <= 1005; i++) {
-        pwr[i] = pwr[i - 1] * 101;
-    }
-
     string s, t;
     ll n, m, q;
     cin >> n >> m >> q;
     cin >> s >> t;
 
-    for(ll i = 0; i < s.length(); i++)
-        hash1[i + 1] = ( hash1[i] * 101) + (s[i] - 'a' + 1);
-
-    for(ll i = 0; i < t.length(); i++)
-        hash2 = (hash2 * 101) + (t[i] - 'a' + 1);
+	findAllOccurances(s , t);
 
     while(q--) {
         ll x, y;
         cin >> x >> y;
-
         ll ans = 0;
-        for(ll i = x; i + t.length() - 1 <= y; i++) {
-            ll hsh = hash1[i + t.length() - 1] - (hash1[i - 1] * pwr[t.length()] );
-            if(hsh == hash2)
+        for(ll i = x; i <= y; i++)
+            if(process[i][y] == 1)
                 ans++;
-        }
-
         cout << ans << endl;
     }
 
