657fdd9ab21bdb6e694864b8f4194b54816c202b
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2019-01-27 04:08:00 +0600

    Add solution for problem `D - Compression` Link: https://codeforces.com/contest/1107/problem/D Tags: dp, implementation, math, number theory, *1800 Ref: https://codeforces.com/contest/1107/submission/49030902

--- codeforces/1107/D.cpp
+++ codeforces/1107/D.cpp
@@ -4,13 +4,47 @@ using namespace std;
 #define ll int
 
 char todo[18][6] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
-int mat[5202][5202];
+int mat[5202][5202], n;
 char inp[1500];
+
+int check(int x)
+
+{
+    ll ni = 1, nj = 1;
+    ll cnti = 0, cntj = 0;
+    for(ll i = 1; i <= n; i++)
+    {
+        nj = 1;
+        cntj = 0;
+
+        cnti++;
+        if(cnti > x)
+        {
+            cnti = 1;
+            ni = i;
+        }
+        for(ll j = 1; j <= n; j++)
+        {
+            cntj++;
+            if (cntj > x)
+            {
+                cntj = 1;
+                nj = j;
+            }
+
+            if(mat[i][j] != mat[ni][nj])
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
 int main()
 
 {
-    ll n, pos;
+    ll pos;
     cin >> n;
     ll rw = n / 4;
     char ch;
@@ -19,7 +53,7 @@ int main()
 
         for(ll j = 0; j < rw; j++) {
             pos = inp[j] - '0';
-            if(isalpha(inp[j]) )
+            if(inp[j] >= 'A' && inp[j] <= 'F')
                 pos = inp[j] - 'A' + 10;
 
             for(ll k = j * 4 + 1, s = 0; s < 4; k++, s++)
@@ -27,83 +61,19 @@ int main()
         }
     }
 
-    ll lo = 1, hi = n, ans = 1, i, j;
-    bool chk = 1;
-    for(ll k = 1; k * k <= n; k++) {
+    for(ll k = n; k > 1; k--) {
 
-        if(n % k != 0)
+        if(n % k)
             continue;
 
-        ll x = n / k, ni = 1, nj = 1;
-        chk = 1;
-        ll cnti = 0, cntj = 0;
-        for(i = 1; i <= n; i++) {
-            nj = 1;
-            cntj = 0;
-
-            cnti++;
-            if(cnti > x) {
-                cnti = 1;
-                ni = i;
-            }
-            for(j = 1; j <= n; j++) {
-                cntj++;
-                if (cntj > x) {
-                    cntj = 1;
-                    nj = j;
-                }
-
-                if(mat[i][j] != mat[ni][nj]) {
-                    chk = 0;
-                    break;
-                }
-            }
-            if(!chk)
-                break;
+        if(check(k)) {
+            printf("%d\n", k);
+            return 0;
         }
 
-        if(chk) {
-            ans = max(ans, x);
-            break;
-        }
-
-        if(k * k == n)
-            continue;
-
-        x = k;
-        chk = 1;
-        cnti = 0, cntj = 0;
-        ni = 1, nj = 1;
-        for(i = 1; i <= n; i++) {
-            nj = 1;
-            cntj = 0;
-
-            cnti++;
-            if (cnti > x) {
-                cnti = 1;
-                ni = i;
-            }
-            for(j = 1; j <= n; j++) {
-                cntj++;
-                if (cntj > x) {
-                    cntj = 1;
-                    nj = j;
-                }
-
-                if(mat[i][j] != mat[ni][nj]) {
-                    chk = 0;
-                    break;
-                }
-            }
-            if(!chk)
-                break;
-        }
-
-        if(chk)
-            ans = max(ans, x);
     }
 
-    cout << ans << endl;
+    printf("1\n");
 
     return 0;
 }
