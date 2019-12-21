5553c0078a4f26fed46b5575fc5cac2cf6f0ecee
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2019-12-21 18:12:00 +0600

    Add solution for problem `B - Modulo Equality` Link: https://codeforces.com/contest/1269/problem/B Tags: brute force, sortings, *1500 Ref: https://codeforces.com/contest/1269/submission/67353604

--- codeforces/1269/B.cpp
+++ codeforces/1269/B.cpp
@@ -26,30 +26,14 @@ using namespace __gnu_pbds;
 #define pf printf
 #define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
 ll a[2009], b[2009];
 
-gp_hash_table <int, int, custom_hash> cnt, freq;
-gp_hash_table <int, int, custom_hash> check;
-gp_hash_table <int, int, custom_hash> :: iterator it;
+//gp_hash_table <ll, ll, custom_hash> cnt, freq;
+//gp_hash_table <ll, ll, custom_hash> check;
 
-//unordered_map <int, int> freq, cnt;
-//unordered_map <int, int> check;
-//unordered_map <int, int> :: iterator it;
+unordered_map <int, int> freq, cnt;
+unordered_map <int, int> check;
+unordered_map <int, int> :: iterator it;
 
 int main()
 
