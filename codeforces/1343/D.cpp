5e74a929d564d87e1f1bc1a53d48ffe6a7f9e621
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2020-04-21 21:59:00 +0600

    Add solution for problem `D - Constant Palindrome Sum` Link: https://codeforces.com/contest/1343/problem/D Tags: brute force, data structures, greedy, two pointers, *1700 Ref: https://codeforces.com/contest/1343/submission/77548077

--- codeforces/1343/D.cpp
+++ codeforces/1343/D.cpp
@@ -29,7 +29,72 @@ using namespace std;
 #define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 const ll sz = 2e5 + 10, tot = 2*sz;
-ll ara[sz], cnt[tot], cnt1[tot];
+ll ara[sz], cnt[tot];
+
+int tree[4*tot], lazy[4*tot];
+
+void build(ll lo, ll hi, ll node)
+{
+    if(lo == hi) {
+        tree[node] = lazy[node] = 0;
+        return;
+    }
+
+    ll mid = (lo+hi) >> 1;
+    build(lo, mid, node<<1);
+    build(mid+1, hi, node<<1 | 1);
+
+    tree[node] = lazy[node] = 0;
+}
+
+void prop(ll lo, ll hi, ll node)
+{
+    if(lo == hi)
+        return;
+
+    ll lft = node << 1, rgt = node << 1 | 1;
+    lazy[lft] += lazy[node], lazy[rgt] += lazy[node];
+
+    ll mid = (lo+hi) >> 1;
+    tree[lft] += ((mid - lo + 1) * lazy[node]);
+    tree[rgt] += ((hi - mid) * lazy[node]);
+
+    lazy[node] = 0;
+}
+
+void update(ll lo, ll hi, ll l, ll r, ll node)
+{
+    if(lazy[node]) prop(lo, hi, node);
+
+    if(lo > r || hi < l)
+        return;
+    if(lo >= l && hi <= r) {
+        tree[node] += (hi - lo + 1);
+        lazy[node]++;
+        return;
+    }
+
+    ll mid = lo+hi >> 1;
+    update(lo, mid, l, r, node<<1);
+    update(mid+1, hi, l, r, node<<1|1);
+
+    tree[node] = tree[(node<<1)] + tree[(node << 1 | 1)];
+}
+
+ll query(ll lo, ll hi, ll idx, ll node)
+{
+    if(lazy[node]) prop(lo, hi, node);
+
+    if(lo > idx || hi < idx)
+        return 0;
+
+    if(lo == hi)
+        return tree[node];
+
+    ll mid = lo+hi >> 1;
+    return query(lo, mid, idx, node<<1)
+         + query(mid + 1, hi, idx, node<<1 | 1);
+}
 
 int main()
 {
@@ -38,6 +103,8 @@ int main()
     while(t--) {
         ll n, k;
         si(n), si(k);
+
+        build(1, 2*k, 1);
 
         for1(i, n) {
             si(ara[i]);
@@ -53,14 +120,13 @@ int main()
             ll mx = max(ara[i], ara[j]);
 
             ll minSum = mn+1, maxSum = mx+k;
-            cnt1[minSum]++, cnt1[maxSum+1]--;
+            update(1, last, minSum, maxSum, 1);
         }
-
-        for1(i, last) cnt1[i] += cnt1[i-1];
 
         ll h = n / 2, ans = 1e9;
         for(ll i = 2; i <= last; i++) {
-            ll zero = cnt[i], one = cnt1[i];
+            ll zero = cnt[i];
+            ll one = query(1, last, i, 1);
             one -= zero;
 
             ll need = one + (h - (zero + one)) * 2;
@@ -69,9 +135,8 @@ int main()
 
         pf("%d\n", ans);
 
-        for1(i, last+1) cnt[i] = cnt1[i] = 0;
+        for1(i, last) cnt[i] = 0;
     }
 
     return 0;
 }
-
