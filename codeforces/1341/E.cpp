7d40742397f13c96cb0bcfbb7b7e58edcc191de4
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2020-04-24 21:03:00 +0600

    Add solution for problem `E - Nastya and Unexpected Guest` Link: https://codeforces.com/contest/1341/problem/E Tags: graphs, implementation, shortest paths, *2400 Ref: https://codeforces.com/contest/1341/submission/77926739

--- codeforces/1341/E.cpp
+++ codeforces/1341/E.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+#define ll int
 #define ull unsigned long long
 #define dd double
 #define ld long double
@@ -12,7 +12,7 @@ using namespace std;
 #define pii pair <int, int>
 #define mp make_pair
 #define pb push_back
-#define inf (1LL << 62)
+#define inf (1LL << 30)
 #define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
 #define for1(i, stop) for(ll i = 1; i <= stop; i++)
 #define for0(i, stop) for(ll i = 0; i < stop; i++)
@@ -26,20 +26,24 @@ using namespace std;
 #define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 const ll sz = 1e4 + 10;
-bool vis[sz][1009];
+ll vis[sz][1009];
 
-ll land[sz];
+int land[sz];
 
 struct node {
-    ll pos, tim, tot;
+    int pos, tim;
+    ll tot;
 };
+const bool operator <(const node &a, const node &b) {
+    return a.tot > b.tot;
+}
 
 int main()
 {
     ll n, m;
     cin >> n >> m;
 
-    for1(i, m) sl(land[i]);
+    for1(i, m) si(land[i]);
     sort(land + 1, land + m+1);
 
     ll g, r;
@@ -52,61 +56,74 @@ int main()
         return 0;
     }
 
-    deque <node> q;
-    q.push_front({1, f, f});
-    vis[1][f] = 1;
+    for0(i, sz) {
+        for(ll j = 0; j <= 1000; j++)
+            vis[i][j] = inf;
+    }
 
-    ll ans = inf;
-    while(!q.empty()) {
-        node u = q.front();
-        q.pop_front();
+    priority_queue <node> pq;
+    pq.push({1, f, f});
+    vis[1][f] = f;
 
-        //cout << u.pos << " " << u.tim << endl;
+    ll ans = inf, tot;
+    int baki, lagbe, lft, rgt, rest, need, now;
+    node u;
+    while(!pq.empty()) {
+        u = pq.top();
+        pq.pop();
+
+        //cout << u.pos << " || " << u.tot << endl;
         if(land[u.pos] == n) {
             ans = min(ans, u.tot);
             continue;
         }
 
-        ll baki = g - u.tim;
+        baki = g - u.tim;
         if(baki == 0) {
             u.tot += r, u.tim = 0;
-            if(!vis[u.pos][u.tim]) {
-                vis[u.pos][u.tim] = 1;
-                q.pb({u.pos, u.tim, u.tot});
-            }
+            if(vis[u.pos][u.tim] <= u.tot)
+                continue;
+
+            vis[u.pos][u.tim] = u.tot;
+            pq.push({u.pos, u.tim, u.tot});
             continue;
         }
-        ll lagbe = n - land[u.pos];
+        lagbe = n - land[u.pos];
         if(lagbe <= baki) {
             ans = min(ans, u.tot + lagbe);
         }
 
-        ll lft = u.pos - 1, rgt = u.pos + 1;
+        if(vis[u.pos][u.tim] < u.tot)
+            continue;
+
+        lft = u.pos - 1, rgt = u.pos + 1;
 
         if(lft >= 1) {
-            ll rest = g - u.tim;
-            ll need = abs(land[u.pos] - land[lft]);
+            rest = g - u.tim;
+            need = abs(land[u.pos] - land[lft]);
             if(need <= rest) {
-                ll now = u.tim + need;
-                ll tot = u.tot + need;
+                now = u.tim + need;
+                tot = u.tot + need;
 
-                if(!vis[lft][now]) {
-                    vis[lft][now] = 1;
-                    q.push_front({lft, now, tot});
+                if(vis[lft][now] > tot) {
+                    vis[lft][now] = tot;
+                    pq.push({lft, now, tot});
                 }
             }
         }
         if(rgt <= n) {
 
-            ll rest = g - u.tim;
-            ll need = abs(land[u.pos] - land[rgt]);
+            rest = g - u.tim;
+            need = abs(land[u.pos] - land[rgt]);
             if(need <= rest) {
-                ll now = u.tim + need;
-                ll tot = u.tot + need;
+                now = u.tim + need;
+                tot = u.tot + need;
 
-                if(!vis[rgt][now]) {
-                    vis[rgt][now] = 1;
-                    q.push_front({rgt, now, tot});
+                //cout << tot <<"   " << vis[rgt][now] << endl;
+                if(vis[rgt][now] > tot) {
+                    vis[rgt][now] = tot;
+                    //cout << tot << endl;
+                    pq.push({rgt, now, tot});
                 }
             }
         }
@@ -119,4 +136,3 @@ int main()
 
     return 0;
 }
-
