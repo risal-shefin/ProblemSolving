53d2aeed316b0160269151bff0e099d474ce04c0
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2020-12-02 20:35:00 +0600

    Add solution for problem `D - Omkar and Bed Wars` Link: https://codeforces.com/contest/1392/problem/D Tags: dp, greedy, *1700 Ref: https://codeforces.com/contest/1392/submission/100199583

---
+++ codeforces/1392/D.cpp
@@ -0,0 +1,73 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+const ll sz = 2e5 + 10;
+char s[sz];
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll n; sl(n);
+        scanf("%s", s+1);
+
+        ll ans = 0;
+        ll l = 1, r = n;
+
+        while(s[r] == s[1] && r >= 1) r--;
+        while(s[l] == s[1] && l < r) l++;
+
+        ll tot = (n-r) + (l-1);
+        ans += tot / 3;
+        if(tot == n && tot % 3 != 0) ans++;
+
+        for(ll i = l; i <= r; i++) {
+
+            ll stop = i;
+            for(ll j = i; j <= r; j++) {
+                if(s[j] != s[i])
+                    break;
+
+                stop = j;
+            }
+
+            ll same = (stop - i + 1);
+            ans += same / 3;
+            i = stop;
+        }
+
+        pf("%lld\n", ans);
+    }
+
+    return 0;
+}
