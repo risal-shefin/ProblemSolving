ec7af8c2f5e3d9784eb3bb6eff66c86903bddd2d
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2020-12-25 17:45:00 +0600

    Add solution for problem `E - Four Segments` Link: https://codeforces.com/contest/1468/problem/E Tags: greedy, *800 Ref: https://codeforces.com/contest/1468/submission/102303075

---
+++ codeforces/1468/E.cpp
@@ -0,0 +1,66 @@
+ // God put a smile upon your face <3
+
+#include <bits/stdc++.h>
+
+#define slld(longvalue) scanf("%lld", &longvalue)
+
+#define ll long long
+#define ull unsigned long long
+#define pll pair < long long, long long >
+
+#define fastio ios_base:: sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+#define pb push_back
+
+#define bug printf("BUG\n")
+
+#define mxlld LLONG_MAX
+#define mnlld -LLONG_MAX
+
+#define mxd 2e8
+#define mnd -2e8
+
+#define pi 3.14159265359
+
+using namespace std;
+
+bool check(ll n, ll pos)
+{
+	return n & (1LL << pos);
+}
+
+ll Set(ll n, ll pos)
+{
+	return n = n | (1LL << pos);
+}
+
+int main()
+{
+    ll i, j, k, l, m, n, o, r, q;
+    ll testcase;
+    ll input, flag, tag, ans;
+
+//    freopen("input.txt", "r", stdin);
+
+//    freopen("output.txt", "w", stdout);
+
+    slld(testcase);
+
+    for(ll cs = 1; cs <= testcase; cs++)
+    {
+		vector < ll > vec;
+		vec.resize(4);
+
+		for(auto &it: vec) cin >> it;
+
+		sort(vec.begin(),vec.end());
+
+		ans = vec[0] * vec[2];
+
+		cout << ans << "\n";
+    }
+
+
+}
+
+
