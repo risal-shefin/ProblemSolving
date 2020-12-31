cb73f6bff5fa4f5df43f78f47614436bf526a099
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2020-12-31 12:49:00 +0600

    Add solution for problem `A - Regular Bracket Sequence` Link: https://codeforces.com/contest/1469/problem/A Tags: constructive algorithms, greedy, *1000 Ref: https://codeforces.com/contest/1469/submission/102888510

---
+++ codeforces/1469/A.cpp
@@ -0,0 +1,59 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+char s[200];
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll p1, p2;
+        scanf("%s", s+1);
+
+        ll len = strlen(s+1);
+
+        if(len & 1) {
+            pf("NO\n");
+            continue;
+        }
+
+        if(s[1] == ')' || s[len] == '(') {
+            pf("NO\n");
+            continue;
+        }
+
+        pf("YES\n");
+    }
+
+    return 0;
+}
