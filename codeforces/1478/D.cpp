8cb642682ff4f96f7d92f9dc3674ff6e3b018fe8
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-01-29 12:04:00 +0600

    Add solution for problem `D - Nezzar and Board` Link: https://codeforces.com/contest/1478/problem/D Tags: constructive algorithms, math, number theory, *1800 Ref: https://codeforces.com/contest/1478/submission/105808638

---
+++ codeforces/1478/D.cpp
@@ -0,0 +1,61 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+const ll sz = 2e5 + 10;
+ll ara[sz];
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll n, k, mn = inf;
+        sl(n), sl(k);
+
+        for1(i, n) {
+            sl(ara[i]);
+            mn = min(mn, ara[i]);
+        }
+
+        ll g = 0;
+        for1(i, n) {
+            ara[i] -= mn;
+            g = __gcd(ara[i], g);
+        }
+        k -= mn;
+
+        if(k%g == 0) pf("YES\n");
+        else pf("NO\n");
+    }
+
+    return 0;
+}
