be61e92ce188a68036213ac0cf83882b3377cbfd
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-02-07 18:36:00 +0600

    Add solution for problem `D - Circle Lattice Points` Link: https://atcoder.jp/contests/abc191/tasks/abc191_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc191/submissions/20035296

---
+++ atcoder/abc191/D.cpp
@@ -0,0 +1,120 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define eps 1e-12
+
+inline ld sq(ld x) {return x*x;}
+
+inline ld dist(ld x1, ld y1, ld x2, ld y2)
+{
+    return sq(x1-x2) + sq(y1-y2);
+}
+
+int main()
+{
+    ld x, y, r;
+    cin >> x >> y >> r;
+
+    ld r2 = r*r;
+
+    ll start = x, ans = 0;
+
+    for(ll xi = start+1; ; xi++) {
+
+        if(dist(xi, y, x, y)-eps > r2)
+            break;
+
+        ll lo = y-1e11, hi = y, mn=1, mx=0;
+
+        while(lo <= hi) {
+            ll mid = lo+hi >> 1;
+
+            if(dist(xi,mid, x, y)-eps > r2)
+                lo = mid + 1;
+            else {
+                hi = mid - 1;
+                mn = mid;
+            }
+        }
+
+        lo = y, hi = y+1e11;
+
+        while(lo <= hi) {
+            ll mid = lo+hi >> 1;
+
+            if(dist(xi, mid, x, y)-eps > r2)
+                hi = mid-1;
+            else {
+                lo = mid + 1;
+                mx = mid;
+            }
+        }
+
+        ans += (mx-mn+1);
+    }
+
+    for(ll xi = start; ; xi--) {
+
+        if(dist(xi, y, x, y)-eps > r2)
+            break;
+
+        ll lo = y-1e11, hi = y, mn=1, mx=0;
+
+        while(lo <= hi) {
+            ll mid = lo+hi >> 1;
+
+            if(dist(xi,mid, x, y)-eps > r2)
+                lo = mid + 1;
+            else {
+                hi = mid - 1;
+                mn = mid;
+            }
+        }
+
+        lo = y, hi = y+1e11;
+
+        while(lo <= hi) {
+            ll mid = lo+hi >> 1;
+
+            if(dist(xi, mid, x, y)-eps > r2)
+                hi = mid-1;
+            else {
+                lo = mid + 1;
+                mx = mid;
+            }
+        }
+
+        ans += (mx-mn+1);
+    }
+
+    cout << ans << EL;
+
+    return 0;
+}
