a91019e5c7fcd2cb7452943c18f6b30804202746
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-02-28 19:38:00 +0600

    Add solution for problem `A - K-th Largest Value` Link: https://codeforces.com/contest/1491/problem/A Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1491/submission/108670585

---
+++ codeforces/1491/A.cpp
@@ -0,0 +1,66 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+const ll sz = 1e5 + 10;
+ll ara[sz];
+
+int main()
+{
+    ll n, q;
+    cin >> n >> q;
+
+    ll o = 0, z= 0;
+    for1(i, n) {
+        ll v; sl(v);
+        ara[i] = v;
+
+        if(v) o++;
+        else z++;
+    }
+
+    while(q--) {
+        ll op, x;
+        sl(op), sl(x);
+
+        if(op == 1) {
+            if(ara[x] == 1) o--, z++;
+            else o++, z--;
+
+            ara[x] = 1-ara[x];
+        }
+        else {
+            if(o >= x) pf("1\n");
+            else pf("0\n");
+        }
+    }
+
+    return 0;
+}
