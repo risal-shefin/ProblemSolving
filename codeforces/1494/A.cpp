362db0ae2513739fa2d1323ec4756751bad476e6
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-03-02 20:51:00 +0600

    Add solution for problem `A - ABC String` Link: https://codeforces.com/contest/1494/problem/A Tags: bitmasks, brute force, implementation, *900 Ref: https://codeforces.com/contest/1494/submission/108887163

---
+++ codeforces/1494/A.cpp
@@ -0,0 +1,98 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    fastio;
+
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        string a; cin >> a;
+
+        if(a[0] == a.back()) {
+            cout << "NO\n";
+            continue;
+        }
+
+        char f = a[0], e = a.back();
+
+        for(char &ch : a) {
+            if(ch == f)
+                ch = '(';
+            else if(ch == e)
+                ch = ')';
+        }
+
+        ll cnt = 0, ok = 1;
+        for(char &ch : a) {
+            if(ch == '(')
+                cnt++;
+            else if(ch == ')')
+                cnt--;
+            else
+                cnt++;
+
+            if(cnt < 0) {
+                ok = 0;
+                break;
+            }
+        }
+        if(cnt != 0) ok = 0;
+
+        if(ok) {
+            cout << "YES\n";
+            continue;
+        }
+
+        cnt = 0, ok = 1;
+        for(char &ch : a) {
+            if(ch == '(')
+                cnt++;
+            else if(ch == ')')
+                cnt--;
+            else
+                cnt--;
+
+            if(cnt < 0) {
+                ok = 0;
+                break;
+            }
+        }
+        if(cnt != 0) ok = 0;
+
+        if(ok) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
