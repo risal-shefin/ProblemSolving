7e30c85c02e2b34583643ca137337b16c59358d3
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-03-24 18:58:00 +0600

    Add solution for problem `D - The Number of Pairs` Link: https://codeforces.com/contest/1499/problem/D Tags: dp, math, number theory, *2100 Ref: https://codeforces.com/contest/1499/submission/110895640

---
+++ codeforces/1499/D.cpp
@@ -0,0 +1,109 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === Debug macro starts here ===
+
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+#else
+#define dbg(x)
+#endif
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+
+// === Debug macro ends here ===
+
+#define ff first
+#define ss second
+
+const ll sz = 2e7 + 10;
+int pcnt[sz], c, d, x;
+bitset <sz> chk;
+
+inline ll solve(ll g)
+{
+    ll k = x/g + d;
+    if(k % c != 0)
+        return 0;
+    k /= c;
+
+    return (1LL << pcnt[k]);
+}
+
+int main()
+{
+    fastio;
+
+    for(int i = 2; i < sz; ++i) {
+        if(!chk[i]) {
+            for(int j = i; j < sz; j += i) {
+                chk[j] = 1;
+                pcnt[j]++;
+            }
+        }
+    }
+
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        cin >> c >> d >> x;
+
+        ll ans = 0;
+
+        for(ll i = 1; i*i <= x; i++) {
+            if(x % i != 0)
+                continue;
+
+            ans += solve(i);
+            if(i != x/i)
+                ans += solve(x/i);
+        }
+
+        cout << ans << EL;
+    }
+
+    return 0;
+}
