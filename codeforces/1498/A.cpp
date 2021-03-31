77c1cc244b30497007c03c5f7b411c1297b466f5
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-03-31 22:38:00 +0600

    Add solution for problem `A - GCD Sum` Link: https://codeforces.com/contest/1498/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1498/submission/111578803

---
+++ codeforces/1498/A.cpp
@@ -0,0 +1,89 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === Debug macro starts here ===
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+#else
+#define dbg(x)
+#endif
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value, Ostream&>::type operator<<(Ostream& os,  const Cont& v){
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+// === Debug macro ends here ===
+
+#define ff first
+#define ss second
+
+bool valid(ll x)
+{
+    ll sum = 0;
+    ll n = x;
+    while(n) {
+        sum += (n%10);
+        n /= 10;
+    }
+    return __gcd(x, sum) != 1;
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--) {
+
+        ll n, ans; sl(n);
+        for(ll i = n; ; i++) {
+            if(valid(i)) {
+                ans = i;
+                break;
+            }
+        }
+
+        pf("%lld\n", ans);
+    }
+
+    return 0;
+}
