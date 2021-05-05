b48e2ce82f44ecc0ce62aba690fc7169fd5420ef
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-05-05 23:35:00 +0600

    Add solution for problem `D - Maximum Sum of Products` Link: https://codeforces.com/contest/1519/problem/D Tags: brute force, dp, implementation, math, two pointers, *1600 Ref: https://codeforces.com/contest/1519/submission/115338971

---
+++ codeforces/1519/D.cpp
@@ -0,0 +1,100 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === Debug macro starts here ===
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === Debug macro ends here ===
+
+#define ff first
+#define ss second
+
+const ll sz = 5e3 + 10;
+ll dp[sz][sz], a[sz], b[sz], pre[sz], suf[sz];
+
+ll solve(ll l, ll r)
+{
+    if(l > r)
+        return 0;
+    if(l == r)
+        return a[l]*b[l];
+
+    ll &ret = dp[l][r];
+    if(ret != -1)
+        return ret;
+
+    return ret = a[l]*b[r] + a[r]*b[l] + solve(l+1, r-1);
+}
+
+int main()
+{
+    ll n; sl(n);
+    for1(i, n) sl(a[i]);
+    for1(i, n) sl(b[i]);
+
+    for1(i, n) pre[i] = pre[i-1] + (a[i] * b[i]);
+    rep1(i, n) suf[i] = suf[i+1] + (a[i] * b[i]);
+
+    ms(dp, -1);
+    ll ans = 0;
+
+    for(ll i = 0; i <= n; i++) {
+        for(ll j = n+1; j > i; j--)
+            ans = max(ans, pre[i] + suf[j] + solve(i+1, j-1));
+    }
+
+    cout << ans << EL;
+
+    return 0;
+}
