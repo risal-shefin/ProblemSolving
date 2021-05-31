06ce110657436c3882eea2e6d0db1d6a5eb6759a
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-05-31 17:33:00 +0600

    Add solution for problem `A - Game of Life` Link: https://codeforces.com/contest/1523/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1523/submission/117984146

---
+++ codeforces/1523/A.cpp
@@ -0,0 +1,98 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === Debug macro starts here ===
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === Debug macro ends here ===
+
+#define ff first
+#define ss second
+
+
+int main()
+{
+    fastio;
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll n, m;
+        cin >> n >> m;
+
+        string s;
+        cin >> s;
+
+        for1(k, m) {
+            vector <int> v;
+
+            for0(i, s.size()) {
+                if(s[i] == '1') continue;
+
+                bool prv = 0, nxt = 0;
+                if(i-1 >= 0 && s[i-1] == '1') prv = 1;
+                if(i+1 < s.size() && s[i+1] == '1') nxt = 1;
+
+                if(prv^nxt) v.pb(i);
+            }
+
+            if(v.empty()) break;
+
+            for(int &x : v) s[x] = '1';
+        }
+        cout << s << EL;
+    }
+
+    return 0;
+}
