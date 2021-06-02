d16799b2acb42270b6bc7063e6d04be0ea24e543
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-06-02 18:28:00 +0600

    Add solution for problem `D - Top Secret Task` Link: https://codeforces.com/contest/590/problem/D Tags: dp, *2300 Ref: https://codeforces.com/contest/590/submission/118192264

---
+++ codeforces/590/D.cpp
@@ -0,0 +1,112 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === Debug macro starts here ===
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === Debug macro ends here ===
+
+#define ff first
+#define ss second
+
+const ll sz = 155;
+ll dp[2][sz][sz*sz], prv[sz][sz*sz], ara[sz];
+
+int main()
+{
+    fastio;
+
+    ll n, k, s, sum = 0;
+    cin >> n >> k >> s;
+
+    for1(i, n) {
+        cin >> ara[i];
+        if(i <= k) sum += ara[i];
+    }
+
+    s = min(s, n*n);
+
+    bool id = 0;
+
+    for(ll i = k+1; i <= n; i++) {
+        id ^= 1;
+
+        for1(j, k) {
+            for0(m, s+1) {
+                dp[id][j][m] = dp[id^1][j][m];
+
+                if(ara[i] >= ara[j])
+                    continue;
+
+                ll need = i-j, prof = ara[j] - ara[i];
+                if(need > m)
+                    continue;
+
+                dp[id][j][m] = max(dp[id][j][m], prof + prv[j+1][m-need]);
+                //dbg(prof);
+            }
+        }
+
+        rep1(j, k)
+            for0(m, s+1)
+                prv[j][m] = max(dp[id][j][m], prv[j+1][m]);
+    }
+
+    ll mx = 0;
+    for0(m, s+1) mx = max(mx, prv[1][m]);
+
+    cout << sum-mx << EL;
+
+    return 0;
+}
