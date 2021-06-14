b914537a1e99768abb67cecc0e42f3afe5ea400e
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-06-14 19:13:00 +0600

    Add solution for problem `D - Antimatter` Link: https://codeforces.com/contest/383/problem/D Tags: dp, *2300 Ref: https://codeforces.com/contest/383/submission/119454921

---
+++ codeforces/383/D.cpp
@@ -0,0 +1,103 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === DEBUG MACRO STARTS HERE === //
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+const ll sz = 1e3 + 10, offset = 1e4, mod = 1e9 + 7;
+ll dp[sz][2*offset+10], ara[sz], n;
+
+ll solve(ll pos, ll sum)
+{
+    if(pos > n)
+        return sum==offset;
+
+    ll &ret = dp[pos][sum];
+    if(ret != -1)
+        return ret;
+
+    ret = (sum==offset);
+
+    ret += solve(pos+1, sum+ara[pos]);
+    if(ret >= mod) ret -= mod;
+
+    ret += solve(pos+1, sum-ara[pos]);
+    if(ret >= mod) ret -= mod;
+
+    return ret;
+}
+
+int main()
+{
+    sl(n);
+    for1(i, n) sl(ara[i]);
+
+    ms(dp, -1);
+    ll ans = 0;
+
+    for1(i, n) {
+        ll got = solve(i, offset);
+        ans += (got-1);
+        if(ans >= mod) ans -= mod;
+    }
+
+    cout << ans << EL;
+
+    return 0;
+}
