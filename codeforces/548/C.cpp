267e527a6545abc9f4a679292134ceb090dbfe23
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-06-21 00:19:00 +0600

    Add solution for problem `C - Mike and Frog` Link: https://codeforces.com/contest/548/problem/C Tags: greedy, math, number theory, *2200 Ref: https://codeforces.com/contest/548/submission/120155375

---
+++ codeforces/548/C.cpp
@@ -0,0 +1,129 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === DEBUG MACRO STARTS HERE === //
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+const ll sz = 1e6 + 10;
+ll tim1[sz], tim2[sz];
+
+int main()
+{
+    ll m, h1, a1, x1, y1, h2, a2, x2, y2;
+    cin >> m >> h1 >> a1 >> x1 >> y1 >> h2 >> a2 >> x2 >> y2;
+
+    ll cur1 = h1, cur2, tim = 0, cyc1 = 0, cyc2 = 0;
+    ms(tim1, -1), ms(tim2, -1);
+
+    while(1) {
+        if(tim1[cur1] != -1) {
+            cyc1 = tim - tim1[cur1];
+            break;
+        }
+
+        tim1[cur1] = tim++;
+        cur1 = (x1*cur1 + y1) % m;
+    }
+
+    cur2 = h2, tim = 0;
+    while(1) {
+        if(tim2[cur2] != -1) {
+            cyc2 = tim - tim2[cur2];
+            break;
+        }
+
+        tim2[cur2] = tim++;
+        cur2 = (x2*cur2 + y2) % m;
+    }
+
+    if(tim1[a1] == -1 || tim2[a2] == -1) {
+        pf("-1\n");
+        return 0;
+    }
+
+    if(tim1[a1] == tim2[a2]) {
+        pf("%lld\n", tim1[a1]);
+        return 0;
+    }
+
+    if(tim1[a1] > tim2[a2])
+        swap(tim1, tim2), swap(a1, a2), swap(cyc1, cyc2), swap(cur1, cur2);
+
+    if(tim1[a1] < tim1[cur1]) {
+        pf("-1\n");
+        return 0;
+    }
+
+    for(ll i = 0; i <= max(cyc1, cyc2); i++) {
+
+        if(i > 0 && tim2[a2] < tim2[cur2])
+            break;
+
+        tim = i*cyc2 + tim2[a2];
+        ll x = (tim-tim1[cur1])%cyc1;
+
+        if(x == (tim1[a1] - tim1[cur1])) {
+            pf("%lld\n", tim);
+            return 0;
+        }
+    }
+    pf("-1\n");
+
+    return 0;
+}
