b8276c96bee2a1e247d9e5acd7175515c80da5a6
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-07-30 20:59:00 +0600

    Add solution for problem `A - PizzaForces` Link: https://codeforces.com/contest/1555/problem/A Tags: brute force, math, *900 Ref: https://codeforces.com/contest/1555/submission/124294130

---
+++ codeforces/1555/A.cpp
@@ -0,0 +1,110 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === DEBUG MACRO STARTS HERE === //
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+ll dp[300];
+
+ll solve(ll n)
+{
+    if(n <= 6) return 15;
+    if(n <= 8) return 20;
+    if(n <= 10) return 25;
+
+    ll &ret = dp[n];
+    if(ret != -1)
+        return ret;
+
+    ret = 15 + solve(n-6);
+    ret = min(ret, 20 + solve(n-8));
+    ret = min(ret, 25 + solve(n-10));
+
+    return ret;
+}
+
+int main()
+{
+    ms(dp, -1);
+
+    dp[0] = 0;
+    for1(i, 240) dp[i] = solve(i);
+
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll n; sl(n);
+
+        if(n <= 240) {
+            pf("%lld\n", dp[n]);
+            continue;
+        }
+
+        ll m = n-120;
+        ll d = 120;
+
+        ll tot = m/d;
+        ll rest = 120 + m%d;
+
+        pf("%lld\n", dp[rest] + (tot*d/6)*15);
+    }
+
+    return 0;
+}
