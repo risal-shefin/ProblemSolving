a557b523058c7711c1474cc81c7133b173f65c84
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-08-01 21:05:00 +0600

    Add solution for problem `C - Web of Lies` Link: https://codeforces.com/contest/1549/problem/C Tags: graphs, greedy, *1400 Ref: https://codeforces.com/contest/1549/submission/124548535

---
+++ codeforces/1549/C.cpp
@@ -0,0 +1,113 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === DEBUG MACRO STARTS HERE === //
+#ifndef ONLINE_JUDGE
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+const ll sz = 2e5 + 10;
+int out[sz];
+
+int main()
+{
+    ll n, m;
+    cin >> n >> m;
+
+    set <int> leaf;
+    for1(i, n) leaf.insert(i);
+
+    for1(i, m) {
+        ll u, v;
+        sl(u), sl(v);
+
+        if(u < v) swap(u, v);
+
+        out[v]++;
+        leaf.erase(v);
+    }
+
+    ll q;
+    sl(q);
+
+    while(q--) {
+        ll op; sl(op);
+
+        if(op == 1) {
+            ll u, v;
+            sl(u), sl(v);
+
+            if(u < v) swap(u, v);
+
+            out[v]++;
+            leaf.erase(v);
+        }
+        else if(op == 2) {
+            ll u, v;
+            sl(u), sl(v);
+
+            if(u < v) swap(u, v);
+
+            out[v]--;
+            if(out[v] == 0) leaf.insert(v);
+        }
+        else
+            pf("%lld\n", (ll)leaf.size());
+    }
+
+    return 0;
+}
