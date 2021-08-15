dafea865c045d9fd930ae2516822afa3c4c99e6d
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-08-15 21:00:00 +0600

    Add solution for problem `C - Mocha and Hiking` Link: https://codeforces.com/contest/1559/problem/C Tags: constructive algorithms, graphs, *1200 Ref: https://codeforces.com/contest/1559/submission/125955497

---
+++ codeforces/1559/C.cpp
@@ -0,0 +1,109 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === DEBUG MACRO STARTS HERE === //
+#ifdef LOCALXOX
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+const ll sz = 1e4 + 10;
+ll ara[sz];
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll n; sl(n);
+        for1(i, n) sl(ara[i]);
+
+        if(ara[1] == 1) {
+            pf("%lld", n+1);
+            for1(i, n) pf(" %lld", i);
+            pn;
+
+            continue;
+        }
+        if(ara[n] == 0) {
+
+            for1(i, n) pf("%lld ", i);
+            pf("%lld", n+1);
+            pn;
+
+            continue;
+        }
+
+        ll f = -1;
+        for1(i, n-1) {
+            if(ara[i] == 0 && ara[i+1] == 1) {
+                f = i;
+                break;
+            }
+        }
+        if(f == -1) {
+            pf("-1\n");
+            continue;
+        }
+
+        for1(i, f) pf("%lld ", i);
+        pf("%lld ", n+1);
+        for(ll i = f+1; i <= n; i++) pf("%lld ", i);
+    }
+
+    return 0;
+}
