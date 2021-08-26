cb5be7f8acec4b110325cd49494f27a6da33f30b
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2021-08-26 21:56:00 +0600

    Add solution for problem `D1 - Two Hundred Twenty One (easy version)` Link: https://codeforces.com/contest/1562/problem/D1 Tags: data structures, dp, math, *1700 Ref: https://codeforces.com/contest/1562/submission/127104451

---
+++ codeforces/1562/D1.cpp
@@ -0,0 +1,97 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// === DEBUG MACRO STARTS HERE === //
+#ifdef LOCALXOX
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+const ll sz = 3e5 + 10;
+char s[sz];
+ll pref[sz];
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--) {
+        ll n, q;
+        sl(n), sl(q);
+        scanf("%s", s+1);
+
+        for1(i, n) {
+            ll cur = (s[i] == '+')? 1 : -1;
+            if(i%2 == 0) cur *= -1;
+
+            pref[i] = cur + pref[i-1];
+        }
+
+        while(q--) {
+            ll l, r;
+            sl(l), sl(r);
+
+            ll sum = abs(pref[r] - pref[l-1]);
+
+            if(sum == 0) pf("0\n");
+            else if(sum & 1) pf("1\n");
+            else pf("2\n");
+        }
+    }
+
+    return 0;
+}
