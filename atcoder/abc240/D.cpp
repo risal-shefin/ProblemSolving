71fc37d39d1d30a51bd4457e8b561bd1b78b96c1
Risal Shahriar Shefin <shefin.cse16@gmail.com>
2022-03-26 16:27:00 +0600

    Add solution for problem `D - Strange Balls` Link: https://atcoder.jp/contests/abc240/tasks/abc240_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc240/submissions/30424351

---
+++ atcoder/abc240/D.cpp
@@ -0,0 +1,103 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sl(n) scanf("%lld", &n)
+#define si(n) scanf("%d", &n)
+#define sd(n) scanf("%lf", &n)
+#define pll pair <ll, ll>
+#define pii pair <int, int>
+#define mp make_pair
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define inf (1LL << 61)
+#define loop(i, start, stop, inc) for(ll i = start; i <= stop; i += inc)
+#define for1(i, stop) for(ll i = 1; i <= stop; ++i)
+#define for0(i, stop) for(ll i = 0; i < stop; ++i)
+#define rep1(i, start) for(ll i = start; i >= 1; --i)
+#define rep0(i, start) for(ll i = (start-1); i >= 0; --i)
+#define ms(n, i) memset(n, i, sizeof(n))
+#define casep(n) printf("Case %lld:", ++n)
+#define pn printf("\n")
+#define pf printf
+#define EL '\n'
+#define fastio std::ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+// returns a random ll integer in [l, r] range
+inline ll gen_random(ll l, ll r) {
+    return uniform_int_distribution<ll>(l, r)(rng);
+}
+
+// === DEBUG MACRO STARTS HERE === //
+#ifdef LOCALXOX
+#define DEBUG
+#define SYS_COL system("COLOR")
+#endif
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; SYS_COL; \
+                cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":" \
+                <<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream,ostream>::value,
+            Ostream&>::type operator<<(Ostream& os,  const Cont& v) {
+	os<<"[";
+	for(auto& x:v){os<<x<<", ";}
+	return os<<"]";
+}
+template<typename Ostream, typename ...Ts>
+Ostream& operator<<(Ostream& os,  const pair<Ts...>& p){
+	return os<<"{"<<p.first<<", "<<p.second<<"}";
+}
+#else
+#define dbg(x)
+#endif
+// === DEBUG MACRO ENDS HERE === //
+
+#define ff first
+#define ss second
+
+const ll sz = 2e5 + 10;
+ll ara[sz];
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    for1(i, n) {
+        sl(ara[i]);
+    }
+
+    cout << 1 << EL;
+    ll tot = 1;
+
+    stack <pii> st;
+    st.push(mp(ara[1], 1));
+
+    for(ll i =2; i <= n; i++) {
+        if(!st.empty() && st.top().ff == ara[i]) st.push(mp(ara[i], st.top().ss+1));
+        else st.push(mp(ara[i], 1));
+
+        tot++;
+
+        if(ara[i] == st.top().ss) {
+            tot -= st.top().ss;
+            while(!st.empty() && st.top().ff == ara[i])
+                st.pop();
+        }
+
+        pf("%lld\n", tot);
+    }
+
+    return 0;
+}
